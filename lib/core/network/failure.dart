136c827d7f7762b3f740d76b483e890ef4cd9857
Mohamed-Bala <mohamed.bala7373@gmail.com>
2024-02-01 16:13:14 +0200

    add data folder ,add model [responses|requests], add repository add logic [cubit], add regex file, in [widget folder] add password-validation, add login bloc listener,add dont-have-account, add terms-and-conditions

---
+++ lib/core/network/failure.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'failure.g.dart';
+
+@JsonSerializable()
+class Failure {
+  final String? message;
+  final int? code;
+
+  Failure({
+    required this.message,
+    this.code,
+  });
+
+  factory Failure.fromJson(Map<String, dynamic> json) =>
+      _$FailureFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FailureToJson(this);
+}
---
+++ lib/features/login/ui/view/login_view.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/helper/spacing.dart';
+import '../../../../core/resources/styles_manager.dart';
+import '../../../../core/widgets/app_text_button.dart';
+import '../../logic/cubit/login_cubit.dart';
+import '../widgets/dont_have_account_text.dart';
+import '../widgets/email_and_password.dart';
+import '../widgets/login_bloc_listener.dart';
+import '../widgets/terms_and_conditions_text.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  bool isObscureText = true;
+  void validateThenDoLogin(BuildContext context) {
+    if (context.read<LoginCubit>().formKey.currentState!.validate()) {
+      context.read<LoginCubit>().emitLogin();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                verticalSpace(40),
+                Text(
+                  'Welcome Back',
+                  style: StylesManager.font30PrimaryBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: StylesManager.font13Grey,
+                ),
+                verticalSpace(36),
+                Column(
+                  children: [
+                    verticalSpace(18),
+                    const EmailAndPassword(),
+                    verticalSpace(24),
+                    Align(
+                      alignment: AlignmentDirectional.centerEnd,
+                      child: Text(
+                        'Forgot Password?',
+                        style: StylesManager.font14Grey.copyWith(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                    verticalSpace(24),
+                    AppTextButton(
+                      buttonText: "Login",
+                      textStyle: StylesManager.font16White,
+                      onPressed: () {
+                        validateThenDoLogin(context);
+                      },
+                    ),
+                    verticalSpace(18),
+                    const TermsAndConditionsText(),
+                    verticalSpace(50),
+                    const DontHaveAccountText(),
+                    const LoginBlocListener(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
