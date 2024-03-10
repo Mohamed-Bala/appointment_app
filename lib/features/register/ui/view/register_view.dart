64e9d0aea2c3b2f7864a52f387c25afcc7eadd00
Mohamed-Bala <mohamed.bala7373@gmail.com>
2024-03-10 16:47:42 +0200

    fix development

--- lib/features/register/ui/view/register_view.dart
+++ lib/features/register/ui/view/register_view.dart
@@ -7,7 +7,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/helper/spacing.dart';
 import '../../../../core/resources/styles_manager.dart';
 import '../../../../core/widgets/app_text_button.dart';
-import '../../../login/ui/widgets/terms_and_conditions_text.dart';
 import '../widgets/already_have_account_text.dart';
 import '../widgets/register_bloc_listener.dart';
 
@@ -60,7 +59,7 @@ class _RegisterViewState extends State<RegisterView> {
                       },
                     ),
                     verticalSpace(18),
-                    const TermsAndConditionsText(),
+                    //  const TermsAndConditionsText(),
                     verticalSpace(40),
                     const AlreadyHaveAccountText(),
                     const RegisterpBlocListener(),
