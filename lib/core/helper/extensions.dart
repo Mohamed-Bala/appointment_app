7ec13e5db0580d636a2cf50c59075de8373bc205
Mohamed-Bala <mohamed.bala7373@gmail.com>
2024-06-13 14:52:47 +0200

    add flutter secure-storage and shared-preferences dependencies,save and secure token,create shared pref file

--- lib/core/helper/extensions.dart
+++ lib/core/helper/extensions.dart
@@ -21,3 +21,8 @@ extension Navigation on BuildContext {
 
   void pop() => Navigator.of(this).pop();
 }
+
+
+extension StringExtension on String? {
+  bool isNullOrEmpty() => this == null || this == "";
+}
